44d63aca674c94e61fcc0c331278d507e2f7598d
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-22 00:08:25 -0300

    alteração no arquivo classe principal.

--- src/App.java
+++ src/App.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        
+
         Cadastro cadastro = new Cadastro();
 
         while (true) {
